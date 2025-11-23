883374ced34fcafcba584326d8f9c981ade57483
bassel11 <basselassad112233@gmail.com>
2025-11-23 12:44:08 +0300

    Meeting Handler

--- CAIT.CMS/Services/Meeting/MeetingApplication/Features/Meetings/Commands/Handlers/CreateMeetingCommandHandler.cs
+++ CAIT.CMS/Services/Meeting/MeetingApplication/Features/Meetings/Commands/Handlers/CreateMeetingCommandHandler.cs
@@ -1,14 +1,35 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
 using MeetingApplication.Features.Meetings.Commands.Models;
 using MeetingApplication.Features.Meetings.Commands.Results;
+using MeetingCore.Repositories;
 
 namespace MeetingApplication.Features.Meetings.Commands.Handlers
 {
     public class CreateMeetingCommandHandler : IRequestHandler<CreateMeetingCommand, MeetingResponse>
     {
+        #region Fields
+
+        private readonly IMeetingRepository _meetingRepository;
+        private readonly IMapper _mapper;
+
+        #endregion
+
+        #region Constructor
+
+        public CreateMeetingCommandHandler(IMeetingRepository meetingRepository, IMapper mapper)
+        {
+            _meetingRepository = meetingRepository;
+            _mapper = mapper;
+        }
+
+        #endregion
+
+        #region Actions
         public Task<MeetingResponse> Handle(CreateMeetingCommand request, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
         }
+        #endregion
     }
 }
