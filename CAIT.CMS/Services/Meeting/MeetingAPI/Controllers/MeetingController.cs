b0223132ba01268d2a575a805a80117fe5616334
bassel11 <basselassad112233@gmail.com>
2025-11-25 18:15:03 +0300

    Building Meeting Controller

---
+++ CAIT.CMS/Services/Meeting/MeetingAPI/Controllers/MeetingController.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MeetingController : ControllerBase
+    {
+        #region Fields
+        private readonly IMediator _mediator;
+        private readonly ILogger<MeetingController> _logger;
+        #endregion
+
+        #region Constructor
+        public MeetingController(IMediator mediator
+                                   , ILogger<MeetingController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+        #endregion
+
+
+
+
+    }
+}
