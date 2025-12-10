668f5c86252cee00e0b22165d132dac95c2a8f3d
bassel11 <basselassad112233@gmail.com>
2025-12-10 17:23:51 +0300

    Update MoMApprovedEvent in Meeting Service to match contracts

--- CAIT.CMS/Services/Meeting/MeetingCore/Events/MoMApprovedEvent.cs
+++ CAIT.CMS/Services/Meeting/MeetingCore/Events/MoMApprovedEvent.cs
@@ -1,17 +1,29 @@
-﻿using BuildingBlocks.Contracts.Common;
+﻿using BuildingBlocks.Contracts.Audit;
+using BuildingBlocks.Contracts.Common;
 
 namespace MeetingCore.Events
 {
-    public class MoMApprovedEvent : IDomainEvent
+    public class MoMApprovedEvent : IDomainEvent, IAuditLogCreated
     {
+        // Domain fields
         public Guid MoMId { get; }
         public Guid MeetingId { get; }
         public Guid ApprovedBy { get; }
         public DateTime ApprovedAt { get; }
 
-        // من IDomainEvent
+        // IDomainEvent
         public Guid EventId { get; } = Guid.NewGuid();
         public DateTime OccurredAt { get; } = DateTime.UtcNow;
+
+        // IAuditLogCreated
+        public string UserId => ApprovedBy.ToString();
+        public string ServiceName => "MeetingService";
+        public string EntityName => "MinutesOfMeeting";
+        public string ActionType => "Approve";
+        public string PrimaryKey => MoMId.ToString();
+        public string? OldValues => null;
+        public string? NewValues => null;
+        public DateTime Timestamp => ApprovedAt;
 
         public MoMApprovedEvent(Guid momId, Guid meetingId, Guid approvedBy, DateTime approvedAt)
         {
@@ -21,5 +33,4 @@ namespace MeetingCore.Events
             ApprovedAt = approvedAt;
         }
     }
-
 }
