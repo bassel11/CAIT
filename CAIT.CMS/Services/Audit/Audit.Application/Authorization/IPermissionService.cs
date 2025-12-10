8731a68aa2b92bc835a3db8901b52ead317c03b7
bassel11 <basselassad112233@gmail.com>
2025-12-10 15:17:36 +0300

    Add Dynamic Authorization Policies and httpclient - PermissionHandler, Requirement

---
+++ CAIT.CMS/Services/Audit/Audit.Application/Authorization/IPermissionService.cs
@@ -0,0 +1,7 @@
+﻿namespace Audit.Application.Authorization
+{
+    public interface IPermissionService
+    {
+        Task<bool> HasPermissionAsync(Guid userId, string permissionName, Guid? resourceId = null);
+    }
+}
---
+++ CAIT.CMS/Services/Audit/Audit.Application/Authorization/PermissionRequirement.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Audit.Application.Authorization
+{
+    public class PermissionRequirement : IAuthorizationRequirement
+    {
+        public string PermissionName { get; }
+        public PermissionRequirement(string permissionName)
+        {
+            PermissionName = permissionName;
+        }
+    }
+}
