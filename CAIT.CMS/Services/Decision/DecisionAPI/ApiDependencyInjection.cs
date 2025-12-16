4d0fdbc7b4e2d8d77d69856575e872b80b6e0fcb
bassel11 <basselassad112233@gmail.com>
2025-12-16 12:48:29 +0300

    Add Healt Checks for Decision API

--- CAIT.CMS/Services/Decision/DecisionAPI/ApiDependencyInjection.cs
+++ CAIT.CMS/Services/Decision/DecisionAPI/ApiDependencyInjection.cs
@@ -1,4 +1,6 @@
 ﻿using BuildingBlocks.Shared.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 namespace DecisionAPI
 {
@@ -7,14 +9,18 @@ namespace DecisionAPI
         public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddExceptionHandler<CustomExceptionHandler>();
-            services.AddHealthChecks();
+            services.AddHealthChecks()
+                .AddSqlServer(configuration.GetConnectionString("DecisionConnectionString")!);
             return services;
         }
 
         public static WebApplication UseApiServices(this WebApplication app)
         {
             app.UseExceptionHandler(options => { });
-            app.UseHealthChecks("/health");
+            app.UseHealthChecks("/health", new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
             return app;
         }
 
